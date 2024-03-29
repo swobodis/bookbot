542cf197f3ae60c7a1ab9a0aa4db0375f04d49c9
swobodis <iswoboda@Isaac-Home-Desktop>
2024-03-29 15:00:27 -0700

    Removed some code which was no longer in use and then added the feature to print out a book report starting with the number of words in the book and then printing out each alpha character onto a new line and it's number of occurances finally ending the report

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import re
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_path = "books/frankenstein.txt"
+    with open(book_path) as f:
         file_contents = f.read()
+        list_of_alpha = count_occurance_of_alpha_chars(file_contents)
         num_words = get_num_words(file_contents)
-        print(f"{num_words} words found in the document")
-        char_occurance = count_occurance_of_chars(file_contents)
-        print("The following are found in the document:")
-        print(char_occurance)
+        print_book_report_on_chars(list_of_alpha, num_words, book_path)
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def count_occurance_of_alpha_chars(text):
+    alpha_chars_list_of_dict = []
+    letter_count_dict = count_occurance_of_chars(text)
+    for char in letter_count_dict:
+        if not char.isalpha():
+            continue
+        alpha_chars_list_of_dict.append({
+            "name": char, 
+            "num_occur":letter_count_dict[char]
+            })
+    return alpha_chars_list_of_dict
+
+def print_book_report_on_chars(list, num_words, book_path):
+    list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    for i in range(len(list)):
+        print(f"The '{list[i]['name']}' character was found {list[i]['num_occur']} times")
+    print("--- End report ---")
 
 def count_occurance_of_chars(text):
     lower_text = text.lower()
@@ -24,6 +43,7 @@ def count_occurance_of_chars(text):
 
     return letter_count_dict
 
-
+def sort_on(dict):
+    return dict["num_occur"]
 
 main()
