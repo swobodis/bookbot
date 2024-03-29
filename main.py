9303e9722aacdd886050a51f9b3cff89f119fd79
swobodis <iswoboda@Isaac-Home-Desktop>
2024-03-29 14:28:08 -0700

    Added functionality to counter the occurance of letters in a book

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import re
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         num_words = get_num_words(file_contents)
         print(f"{num_words} words found in the document")
+        char_occurance = count_occurance_of_chars(file_contents)
+        print("The following are found in the document:")
+        print(char_occurance)
 
 def get_num_words(text):
     words = text.split()
@@ -10,7 +15,15 @@ def get_num_words(text):
 
 def count_occurance_of_chars(text):
     lower_text = text.lower()
-    
+    letter_count_dict = {}
+    for i in range(len(lower_text)):
+        if lower_text[i] in letter_count_dict:
+            letter_count_dict[lower_text[i]] += 1
+        else:
+            letter_count_dict[lower_text[i]] = 1
+
+    return letter_count_dict
+
 
 
 main()
